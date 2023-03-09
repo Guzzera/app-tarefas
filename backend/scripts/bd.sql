1e564fdba747eb6339104e699e8fd2636cb60cee
gustavo <guzzeradeveloper@gmail.com>
2023-03-09 15:44:26 -0300

    "CRUD"

--- backend/scripts/bd.sql
+++ backend/scripts/bd.sql
@@ -2,13 +2,13 @@ CREATE DATABASE IF NOT EXISTS tarefas_bd;
 
 USE tarefas_bd;
 
-CREATE TABLE IF NOT EXISTS tarefas_bd (
+CREATE TABLE IF NOT EXISTS tarefas(
     id INT NOT NULL AUTO_INCREMENT,
-    titulo VARCHAR(100) NOT NULL,
+    titulo VARCHAR(100),
     descricao TEXT,
     PRIMARY KEY (id)
 );
 
-INSERT INTO tarefas_bd (titulo, descricao) VALUES
+INSERT INTO tarefas(titulo, descricao) VALUES
     ('tarefa 1', 'descricao'),
     ('tarefa 2', 'descricao 2');
--- backend/src/controllers/TarefaController.js
+++ backend/src/controllers/TarefaController.js
@@ -1,51 +1,58 @@
-import { conectar } from '../BancoDados';
+import { conectar } from "../BancoDados";
 
-export const getTarefas = async (req, ret) => {
-    const conexao = await conectar();
-    const [rows] = await conexao.query("SELECT * FROM tarefas_bd");
-    ret.json(rows);
-};
-
-export const getTarefa = async (req, ret) => {
-    const conexao = await conectar();
-    const [rows] = await conexao.query("SELECT * FROM tarefas_bd WHERE id =?", [
-        req.params.id
-    ]);
-
-    ret.json(rows[0]);
-};
-
-export const saveTarefa = async (req, ret) => {
-    const conexao = await conectar();
-    const [resultado] = await conexao.query("INSERT INTO tarefas_bd(titulo, descricao) VALUES (?,?)",[
-        req.body.titulo,
-        req.body.descricao
-    ]);
-    ret.json({
-        id: resultado.insertId,
-        ...req.body,
-    });
-};
-
-export const deleteTarefa = async (req, ret) => {
-    const conexao = await conectar();
-    await conexao.query("DELETE FROM tarefas_bd WHERE id = ?", [
-        req.params.id,
-    ]);
-    ret.sendStatus(204);
-};
-
-export const updateTarefa = async (req, ret) => {
-    const conexao = await conectar();
-    await conexao.query("UPDATE tarefas_bd SET ? WHERE id =?", [
-        req.body,
-        req.params.id
-    ]);
-    ret.sendStatus(204)
-};
-
-export const getTarefasCount = async (req, ret) => {
+export const getTarefas = async (req, res) => {
   const conexao = await conectar();
-  const [rows] = await conexao.execute("SELECT COUNT(*) FROM tarefas_bd");
-  ret.json(rows[0]["COUNT(*)"]);
+  const [rows] = await conexao.execute("SELECT * FROM tarefas");
+  res.json(rows);
+};
+
+export const saveTarefa = async (req, res) => {
+  try {
+    const conexao = await conectar();
+    const [resultado] = await conexao.execute(
+      "INSERT INTO tarefas(titulo, descricao) VALUES (?, ?)",
+      [req.body.titulo, req.body.descricao]
+    );
+
+    const novoUsuario = {
+      id: resultado.insertId,
+      ...req.body,
+    };
+    res.json(novoUsuario);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const getTarefa = async (req, res) => {
+  const conexao = await conectar();
+  const rows = await conexao.execute("SELECT * FROM tarefas WHERE id = ?", [
+    req.params.id,
+  ]);
+  res.json(rows[0][0]);
+};
+
+export const deleteTarefa = async (req, res) => {
+  const conexao = await conectar();
+  const result = await conexao.execute("DELETE FROM tarefas WHERE id = ?", [
+    req.params.id,
+  ]);
+  console.log(result);
+
+  res.sendStatus(204);
+};
+
+export const updateTarefa = async (req, res) => {
+  const conexao = await conectar();
+  await conexao.query("UPDATE tarefas SET ? WHERE id = ?", [
+    req.body,
+    req.params.id,
+  ]);
+  res.sendStatus(204);
+};
+
+export const getTarefaCount = async (req, res) => {
+  const conexao = await conectar();
+  const [rows] = await conexao.execute("SELECT COUNT(*) FROM tarefas");
+  res.json(rows[0]["COUNT(*)"]);
 };
